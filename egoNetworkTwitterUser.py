618555a87fb1595f91e0a6562675b1dbe8ee4744
Hend Alrasheed <hend4200@gmail.com>
2019-12-07 17:34:05 +0300

    Create egoNetworkTwitterUser.py

---
+++ egoNetworkTwitterUser.py
@@ -0,0 +1,114 @@
+# --------------------------------------------- #
+# --- prepared by: Hend Alrasheed - halrasheed@ksu.edu.sa --- #
+# --- Oct 2019 --- #
+# --- The code returns the ego network of a given twitter user. The network will 
+# --- be returned in a text file (directed edge list) format.
+# --------------------------------------------- #
+
+import tweepy
+import time
+import sys
+import os
+import unicodedata
+
+#----------------------------------------------------
+
+
+def load_first_degree_followers(user):
+
+    ids = []
+    for page in tweepy.Cursor(api.followers_ids, screen_name=user).pages():
+       ids.extend(page)
+       time.sleep(60)
+
+    for i in ids:
+        f.write(str(i) + "  " + str(user_id) + "\n")
+
+    print 'Done with the 1st degree followers'
+
+    return ids
+
+#----------------------------------------------------
+
+def load_first_degree_friends(user):
+
+    ids = []
+    for page in tweepy.Cursor(api.friends_ids, screen_name=user).pages():
+       ids.extend(page)
+       time.sleep(60)
+
+    for i in ids:
+        f.write(str(user_id) + "  " + str(i) + "\n")
+
+    print 'Done with the 1st degree freinds'
+
+    return ids
+
+#----------------------------------------------------
+
+def load_second_degree_followers(users):
+
+    ids = []
+    for user in users:
+        try:
+            if api.get_user(user).protected == False:
+               ids = []
+               for page in tweepy.Cursor(api.followers_ids, id=user).pages():
+                   ids.extend(page)
+                   time.sleep(60)
+
+               for i in ids:
+                   f.write(str(i) + "  " + str(user) + "\n")
+
+        except Exception:
+             pass 
+
+    print 'Done with the 2nd degree followers'
+    
+#----------------------------------------------------
+
+def load_second_degree_friends(users):
+
+    ids = []
+    for user in users:
+        try:
+            if api.get_user(user).protected == False:         
+               ids = []
+               for page in tweepy.Cursor(api.friends_ids, id=user).pages():
+                   ids.extend(page)
+                   time.sleep(60)
+
+               for i in ids:
+                   f.write(str(user) + "  " + str(i) + "\n")
+                  
+        except Exception:
+             pass            
+
+    print 'Done with the 2nd degree friends'
+    
+
+#----------------------------------------------------
+
+CONSUMER_KEY    = 'ytL7IqHlwowHYctEh8nUnbMyL'
+CONSUMER_SECRET = 'yvYR2QXuNBSJLWqIYFBhJY38mRVwYlQvJWpkFvCSUFjIrzMUZM'
+ACCESS_KEY      = '446908951-Q2zVmm7yJmgqU8HgRCsXJrhY8BVlaskXcrvPVJN6'
+ACCESS_SECRET   = '3XFMk3Bx2zFvWT4luKTql8gWTDmvUygTAJ3DjPrlXjuEu'
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+username = sys.argv[1]
+user = api.get_user(username) 
+user_id = user.id
+print user.id
+f  = open(username+'-ego-network.txt', 'w')
+
+print 'Retrieving user data...'
+first_degree_followers = load_first_degree_followers(username)
+first_degree_friends   = load_first_degree_friends(username)
+load_second_degree_followers(first_degree_followers)
+load_second_degree_friends(first_degree_friends)
+
+
+f.close()
+print 'Ego network file is ready...'
